bb9b37a0ea1caa6858b594b1c4749c6c23b4efa9
Curtis Fisher <KnowIsPow@users.noreply.github.com>
2021-11-17 11:05:35 -0500

    Update README and index page with basic boilerplate information

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# NEXT.JS + TAILWINDCSS STARTER KIT
+
+An opinionated boilerplate for working with Next.js and TailwindCSS. The purpose of this work is to provide a framework and set of components that make it convienent to start new Next.js + TailwindCSS projects.
+
 ## Getting Started
 
 Run the following command to create a new project with this Starter:
@@ -7,3 +11,31 @@ yarn create next-app [project-name] -e https://github.com/KnowIsPow/next-starter
 # or
 npx create-next-app [project-name] -e https://github.com/KnowIsPow/next-starter-tailwind
 ```
+
+## FOLDER STRUCTURE
+
+All files are moved into the ./src folder. Additional folders included:
+
+### UI
+
+A place for generic design components. UI components are generally useable in a variety of context and in many cases across multiple different applications. An example of a UI component is a generic calendar that can be used for many purposes.
+
+### FEATURES
+
+A place for application-specific functionality. Features tend to be comprised of UI building blocks and include some form of state. An example of a feature would be an hotel availability calendar that adds room availability state to a calendar UI component.
+
+### FUNCTIONS
+
+A place for functions that can be used in multiple contexts. The included example is a function that formats numbers by adding commas to them.
+
+## NAMING CONVENTION
+
+All files are named in lowercase-kebab-case.
+
+## TAILWINDCSS COMPONENTS
+
+Styled components provided in styles.css serve to provide a set of basic components that promote building a cohesive brand.
+
+## REACT COMPONENTS
+
+React components are added as a need is recognized to reduce development around commonly recurring component patterns.
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,73 @@ import Page from 'features/page';
 
 export default function IndexPage() {
   return (
-    <Page title="Index Page" description="Index page description" canonical="/">
-      <h1>Index Page</h1>
+    <Page
+      title="Next.js Tailwind Starter"
+      description="An opinionated starter kit for Next.js and TailwindCSS"
+      canonical="/"
+    >
+      <main className="space-y-10 py-12 md:py-24 bg-gray-50">
+        <div className="section pb-8 border-b">
+          <h1 className="display text-xl md:text-2xl lg:text-3xl">
+            Next.js + TailwindCSS Starter Kit
+          </h1>
+          <p className="body mt-2">
+            An opinionated boilerplate for working with Next.js and TailwindCSS. The purpose of this
+            work is to provide a framework and set of components that make it convienent to start
+            new Next.js + TailwindCSS projects.
+          </p>
+        </div>
+        <div className="section">
+          <div className="shadow p-4 bg-white">
+            <h2 className="display text-xl">Folder Structure</h2>
+            <p className="body mt-2">
+              All files are moved into the ./src folder. Additional folders included:
+            </p>
+            <h3 className="display mt-4">UI</h3>
+            <p className="body mt-2">
+              A place for generic design components. UI components are generally useable in a
+              variety of context and in many cases across multiple different applications. An
+              example of a UI component is a generic calendar that can be used for many purposes.
+            </p>
+            <h3 className="display mt-4">Features</h3>
+            <p className="body mt-2">
+              A place for application-specific functionality. Features tend to be comprised of UI
+              building blocks and include some form of state. An example of a feature would be an
+              hotel availability calendar that adds room availability state to a calendar UI
+              component.
+            </p>
+            <h3 className="display mt-4">Functions</h3>
+            <p className="body mt-2">
+              A place for functions that can be used in multiple contexts. The included example is a
+              function that formats numbers by adding commas to them.
+            </p>
+          </div>
+        </div>
+        <div className="section">
+          <div className="shadow p-4 bg-white">
+            <h2 className="display text-xl">Naming Convention</h2>
+            <p className="body mt-2">All files are named in lowercase-kebab-case. </p>
+          </div>
+        </div>
+        <div className="section">
+          <div className="shadow p-4 bg-white">
+            <h2 className="display text-xl">TailwindCSS Components</h2>
+            <p className="body mt-2">
+              Styled components provided in styles.css serve to provide a set of basic components
+              that promote building a cohesive brand.
+            </p>
+          </div>
+        </div>
+        <div className="section">
+          <div className="shadow p-4 bg-white">
+            <h2 className="display text-xl">React Components</h2>
+            <p className="body mt-2">
+              React components are added as a need is recognized to reduce development around
+              commonly recurring component patterns.
+            </p>
+          </div>
+        </div>
+      </main>
     </Page>
   );
 }

145f6f3079519acc21ab3d55e326a99e3625b290
Curtis Fisher <KnowIsPow@users.noreply.github.com>
2021-11-17 11:05:17 -0500

    Add basic 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,17 @@
 import Page from 'features/page';
+import Button from 'ui/form/button';
+import Link from 'ui/link';
 
 export default function NotFoundPage() {
   return (
     <Page title="404 Page" noindex>
-      <h1>404 Not Found</h1>
+      <main className="section py-4 md:py-12 lg:py-24">
+        <h1 className="display text-xl md:text-2xl lg:text-3xl">Page Not Found</h1>
+        <p className="body mt-2">This page does not exist.</p>
+        <Link href="/">
+          <Button className="btn-primary mt-4">Go To Home Page</Button>
+        </Link>
+      </main>
     </Page>
   );
 }
